b959f7eb76042e5b2c812aedb94442454eac727c
prashant.basnet@selu.edu <prashant.basnet@selu.edu>
2024-05-21 14:22:00 -0400

    setting up route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    images: {
+        domains: ['images.unsplash.com', 'dummyimage.com'], // Add 'dummyimage.com' to the list
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'images.unsplash.com', // Correct the hostname here
+            port: '',
+            pathname: '/account123/**',
+          },
+        ],
+    },
+};
 export default nextConfig;
